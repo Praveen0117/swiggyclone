5686178a3668845258c3d6ffbb6d60517dc59d74
Praveen0117 <polakipraveenkumar9@gmail.com>
2024-07-08 10:20:24 +0530

    2nd Commit

--- src/Cart.js
+++ src/Cart.js
@@ -4,7 +4,6 @@ import {clearCart} from "../utils/cartSlice"
 
 const Cart = () => {
   const cartItems = useSelector((store) => store.cart.items);
-//   console.log(cartItems);
 
 
 
@@ -41,40 +40,42 @@ const Cart = () => {
   };
 
   return (
-    <div className="flex flex-col justify-items-center w-6/12 items-center m-4  p-4 ">
+    <div className="flex flex-col w-6/12 items-center m-4  p-4 ">
       <h1 className="font-serif font-bold text-base m-5 mt-0">Cart Items</h1>
-    <div className="">
-      {renderBillDetails()}
-      <button className="p-2 mt-5 bg-black text-white rounded-lg" onClick={handleClearCart}>Clear Cart</button>
-      {cartItems.length == 0 && <h1>Cart is Empty Add Items to the Cart</h1>}
-      <div className="m-auto ">
-        {cartItems.map((item) => (
-          <div
-            key={item.card.info.id}
-            className="p-2 m-2 border-b-2 h-auto border-black text-left flex justify-between"
-          >
-            <div className=" mr-10 w-9/12">
-              <div className="py-2">
-                <span>{item.card.info.name}</span>
-                <span> - ₹{item.card.info.price / 100}</span>
+      {cartItems.length == 0 ? <h1>Cart is Empty Add Items to the Cart</h1> :
+        <div className="">
+          {renderBillDetails()}
+          <button className="p-2 mt-5 bg-black text-white rounded-lg" onClick={handleClearCart}>Clear Cart</button>
+          
+          <div className="m-auto ">
+            {cartItems.map((item) => (
+              <div
+                key={item.card.info.id}
+                className="p-2 m-2 border-b-2 h-auto border-black text-left flex justify-between"
+              >
+                <div className=" mr-10 w-9/12">
+                  <div className="py-2">
+                    <span>{item.card.info.name}</span>
+                    <span> - ₹{item.card.info.price / 100}</span>
+                  </div>
+                  <p className="text-xs">{item.card.info.description}</p>
+                </div>
+                <div className="w-3/12">
+                  <img
+                    src={
+                      "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/" +
+                      item.card.info.imageId
+                    }
+                    className="rounded-r-lg w-28 h-28 mx-24 my-4"
+                  />
+                  
+                </div>
               </div>
-              <p className="text-xs">{item.card.info.description}</p>
-            </div>
-            <div className="w-3/12">
-              <img
-                src={
-                  "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/" +
-                  item.card.info.imageId
-                }
-                className="rounded-r-lg w-28 h-28 mx-24 my-4"
-              />
-              
-            </div>
+            ))}
           </div>
-        ))}
-      </div>
-      
+          
         </div>
+      }
     </div>
   );
 };
